649fbc3c718418df5158d39e5a98a689cdd7f99a
Dotun Ogunsakin <email@dotun.me>
2025-09-24 23:36:35 -0400

    Request handling fixes, build process improvements

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,49 +6,65 @@ dotenv.config();
 
 interface ClientQuery {
   query: string;
-}
-
-interface ClientResponse {
-  content: string;
+  model: string;
 }
 
 const headers = {
-  "Access-Control-Allow-Origin": process.env.CLIENT_HOST as string,
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": process.env.CLIENT_HOST_PROD as string,
   "Access-Control-Allow-Methods": "POST, OPTIONS"
 }
 
 export async function handler(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
-  if(!event.body) {
+  try {
+    if(!event.body) {
+      return {
+        statusCode: 400,
+        headers,
+        body: JSON.stringify({ error: "No data was provided." })
+      }
+    }
+
+    console.log("[OLLAMA_CHAT_API::INFO]", event.body);
+
+    // Allow dev testing
+    if(event.headers.Origin === process.env.CLIENT_HOST_DEV) {
+      headers["Access-Control-Allow-Origin"] = process.env.CLIENT_HOST_DEV as string;
+    }
+
+    const requestPayload = JSON.parse(event.body) as ClientQuery;
+    let result = "";
+    
+    const ollama = new Ollama({
+      host: process.env.OLLAMA_API_HOST,
+      headers: {
+        Authorization: `Bearer ${process.env.OLLAMA_API_KEY}`,
+      },
+    });
+
+    const ollamaResponse = await ollama.chat({
+      model: requestPayload.model,
+      messages: [{ role: "user", content: requestPayload.query }],
+      stream: true,
+    });
+
+    for await (const part of ollamaResponse) {
+      result += part.message.content;
+    }
+
     return {
-      statusCode: 400,
+      statusCode: 200,
       headers,
-      body: JSON.stringify({ error: "No query was provided." })
+      body: JSON.stringify({ content: result })
     }
   }
+  catch(e) {
+    console.log("[OLLAMA_CHAT_API::ERROR]", e);
 
-  const clientQuery = (JSON.stringify(event.body) as unknown as ClientQuery).query;
-  const result: ClientResponse[] = [];
-  
-  const ollama = new Ollama({
-    host: process.env.OLLAMA_API_HOST,
-    headers: {
-      Authorization: `Bearer ${process.env.OLLAMA_API_KEY}`,
-    },
-  });
-
-  const response = await ollama.chat({
-    model: "gpt-oss:120b",
-    messages: [{ role: "user", content: clientQuery }],
-    stream: true,
-  });
-
-  for await (const part of response) {
-    result.push({ content: part.message.content });
-  }
-
-  return {
-    statusCode: 200,
-    headers,
-    body: JSON.stringify(response)
+    return {
+      statusCode: 401,
+      headers,
+      body: JSON.stringify({ error: "There was an error." })
+    }
   }
 }
